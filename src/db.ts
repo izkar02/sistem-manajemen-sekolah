5b32c64cef89e058488f6541e46bc81e4812d227
izkar02 <izaq.zulfykar@gmail.com>
2026-08-26 06:26:22 +0800

    Add MySQL SSL configuration

--- src/db.ts
+++ src/db.ts
@@ -1,4 +1,5 @@
 // src/db.ts
+
 import mysql from "mysql2/promise";
 
 const {
@@ -7,6 +8,7 @@ const {
   DB_USER = "root",
   DB_PASSWORD = "",
   DB_NAME = "sistem_manajemen_sekolah",
+  DB_SSL_CA,
 } = process.env;
 
 export const db = mysql.createPool({
@@ -15,6 +17,14 @@ export const db = mysql.createPool({
   user: DB_USER,
   password: DB_PASSWORD,
   database: DB_NAME,
+
+  ssl: DB_SSL_CA
+    ? {
+        ca: DB_SSL_CA,
+        rejectUnauthorized: true,
+      }
+    : undefined,
+
   waitForConnections: true,
   connectionLimit: 10,
 });
